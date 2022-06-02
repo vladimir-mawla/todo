8848792a008ad854293407de8d6f93a8ca901fe7
vladimir <vladimirmawla2@gmail.com>
2022-06-02 16:57:16 +0300

    Moving time variables outside the event listener

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,19 @@
-$(".submit").on("click",function(){
-    if($(".title").val() =="" || $(".description").val() ==""){
+var date = new Date();
+var month = date.getMonth();
+var day = date.getDate();
+var hr = date.getHours();
+var min = date.getMinutes();
+var sec = date.getSeconds();
+var todo=$("<div class='todo'></div>").text($(".title").val() + " " + $(".description").val());
+var a = "agg"
+
+var time = date.getFullYear() + '-' + month + '-' + day + " " + hr + ":" + min + ":" + sec;
+var show_time=$("<div class='time'></div>")
+$("#submit").on("click",function(){
+    if($(".title").val() === "" || $(".description").val() === ""){
         alert("Empty todo")
     }else{
-        var date = new Date();
-        var month = date.getMonth();
-        var day = date.getDate();
-        var hr = date.getHours();
-        var min = date.getMinutes();
-        var sec = date.getSeconds();
-
-        var time = date.getFullYear() + '-' + month + '-' + day + " " + hr + ":" + min + ":" + sec;
-        var show_time=$("<div class='time'></div>")
+        
         show_time.append(time)
     }
 
