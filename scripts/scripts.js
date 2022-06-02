f406d670fdf63d3ab5b2b103ee92852d6ea6f41a
vladimir <vladimirmawla2@gmail.com>
2022-06-02 15:16:24 +0300

    Adding new date function and assigning variables for its units

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,12 @@ $(".submit").on("click",function(){
     if($(".title").val() =="" || $(".description").val() ==""){
         alert("Empty todo")
     }else{
-        
+        var date = new Date();
+        var month = date.getMonth();
+        var day = date.getDate();
+        var c=date.getHours();
+        var m=date.getMinutes();
+        var s=date.getSeconds(); 
     }
 
 })
